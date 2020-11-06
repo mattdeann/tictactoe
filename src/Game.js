de2024643bc01b17599444623564173c7ac98974
mattdean <matthewallandean@gmail.com>
2020-11-05 20:51:58 -0700

    Rename js files to match appropriate style

---
+++ src/Game.js
@@ -0,0 +1,7 @@
+//Make an array containing nine spaces
+// make a function that will push to correct space depending on player input
+// assess whether or not game was won or tied
+// switch player who is allowed to run function
+
+
+class Game 
